94a77e7c08d489faaa20182afbe2869ee42512b2
bahaa <113281341+bahaamezher3@users.noreply.github.com>
2025-08-30 08:18:05 +0300

    new

--- so_long.c
+++ so_long.c
@@ -1,71 +1,106 @@
 #include "so_long.h"
 
-int ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 void map_dimentioner(char *filename, int *a, int *b)
 {
-	int		fd;
-	int		i;
-	int		j;
-	int		line_Count;
-	
-	line_Count = 0;
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		exit(1);
-	i = ft_strlen(get_next_line(fd));
-	j = i;
-	while (i = ft_strlen(get_next_line(fd)))
-	{
-		if(i != j)
-		{
-			perror("Map Error");
-			exit(1);
-		}
-		j = i;
-		line_Count++;
-	}
-	*a = line_Count;
-	*b = i; 
+    int     fd;
+    int     i;
+    int     j;
+    int     line_count;
+    char    *tmp;
+    
+    line_count = 1;
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        exit(1);
+    tmp = get_next_line(fd);
+    if (!tmp)
+        exit(1);
+    i = ft_strlen(tmp);
+    j = i;
+    free(tmp);
+    while (1)
+    {
+        tmp = get_next_line(fd);
+        if (!tmp)
+            break;
+        i = ft_strlen(tmp);
+        if (i != j)
+        {
+            free(tmp);
+            perror("Map Error");
+            exit(1);
+        }
+        j = i;
+        line_count++;
+        free(tmp);
+    }
+    close(fd);
+    *a = line_count;
+    *b = i; 
+}
+
+void freer(char **map, int i)
+{
+    int k = 0;
+    while (k < i)
+    {
+        free(map[k]);
+        k++;
+    }
+    free(map);
 }
 
 char **read_map_with_error_handle(char *filename)
 {
-	int		fd;
-	char	**map_fill;
-	int		max_i;
-	int		max_j;
+    int     fd;
+    char    **map_fill;
+    int     max_i;
+    int     max_j;
+    int     i;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		exit(1);
-	max_i = 0;
-	max_j = 0;
-	map_dimentioner(filename, &max_i, &max_j);
+    map_dimentioner(filename, &max_i, &max_j);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        exit(1);
 
+    map_fill = malloc(sizeof(char *) * (max_i + 1));
+    if (!map_fill)
+        exit(2);
+
+    i = 0;
+    while (i < max_i)
+    {
+        map_fill[i] = get_next_line(fd);
+        if (!map_fill[i])
+        {
+            freer(map_fill, i);
+            close(fd);
+            exit(3);
+        }
+        i++;
+    }
+    map_fill[i] = NULL;
+    close(fd);
+    return (map_fill);
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	char	**map;
+    char    **map;
+    int     i = 0;
 
-	if (argc != 2)
-	{
-		write(1, "Usage: ./so_long map.ber\n", 26);
-		return (1);
-	}
-	if (open(argv[1], O_RDONLY) == -1)
-	{
-		write(1, "Error Opening map.ber\n", 23);
-		return (1);
-	}
-	map = read_map_with_error_handle(argv[1]);
+    if (argc != 2)
+    {
+        write(1, "Usage: ./so_long map.ber\n", 26);
+        return (1);
+    }
+    if (open(argv[1], O_RDONLY) == -1)
+    {
+        write(1, "Error Opening map.ber\n", 23);
+        return (1);
+    }
+    map = read_map_with_error_handle(argv[1]);
+
+    freer(map, i);
+    return (0);
 }
